51542cf6c0a5b057b60cf52ba097d5da57d1247c
tienak1 <thattien1111@gmail.com>
2021-11-16 00:40:05 +0700

    e

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Asbab' });
+});
+
+router.get('/login.html', function(req, res, next) {
+  res.render('login', { title: 'Asbab - Login' });
+});
+
+router.get('/itemList', function(req, res, next) {
+  res.render('itemList', { title: 'Asbab - Item' });
+});
+
+router.get('/itemEditor', function(req, res, next) {
+  res.render('itemEditor', { title: 'Asbab - Item' });
+});
+
+router.get('/charts.html', function(req, res, next) {
+  res.render('charts', { title: 'Asbab - Item' });
+});
+
+router.get('/elements.html', function(req, res, next) {
+  res.render('elements', { title: 'Asbab - Item' });
+});
+
+module.exports = router;
